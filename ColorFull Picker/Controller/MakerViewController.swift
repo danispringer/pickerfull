38e9879755f6ddcc2a88db754aa0b0ccc8f8db3a
Dani Springer <musicbyds@icloud.com>
2018-07-15 06:35:28 +0200

    refactor: create non-functioning keyboard buttons

--- ColorFull Picker/Controller/MakerViewController.swift
+++ ColorFull Picker/Controller/MakerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MakerViewController: UIViewController, UITextFieldDelegate {
+class MakerViewController: UIViewController {
 
     // MARK: Outlets
     
@@ -16,11 +16,7 @@ class MakerViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var greenControl: UISlider!
     @IBOutlet weak var blueControl: UISlider!
     
-    @IBOutlet weak var userTextField: UITextField!
-    @IBOutlet weak var warningLabel: UILabel! // TODO: to remove after customising keyboard
-    @IBOutlet weak var infoTextView: UITextView! // TODO: to remove after creating tutorial page
-    
-    
+
     // MARK: placeholders
     
     var currentUIColor: UIColor!
@@ -31,8 +27,7 @@ class MakerViewController: UIViewController, UITextFieldDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        
-        subscribeToKeyboardNotifications()
+
     }
     
     
@@ -44,7 +39,8 @@ class MakerViewController: UIViewController, UITextFieldDelegate {
         greenControl.minimumTrackTintColor = UIColor.green
         blueControl.minimumTrackTintColor = UIColor.blue
         
-        userTextField.text = "E57BF2"
+        //        userTextField.text = "E57BF2" // TODO: add text to eventual label
+        
         // update controls with animation
         let redHex = Float("0xE5")! / 255
         let greenHex = Float("0x7B")! / 255
@@ -57,28 +53,14 @@ class MakerViewController: UIViewController, UITextFieldDelegate {
             self.view.backgroundColor = UIColor(red: CGFloat(self.redControl.value), green: CGFloat(self.greenControl.value), blue: CGFloat(self.blueControl.value), alpha: 1)
         })
         
+        //        infoTextView.text = "What can you do?\n\u{2022} Drag the sliders to get a colour you like, and copy the generated HEX code to save your lovely blend.\n\u{2022} Edit the code to preview any colour from millions of choices." // TODO: move to tutorial view
         
-        
-        
-        
-        warningLabel.isHidden = true
-        infoTextView.text = "What can you do?\n\u{2022} Drag the sliders to get a colour you like, and copy the generated HEX code to save your lovely blend.\n\u{2022} Edit the code to preview any colour from millions of choices."
-        infoTextView.contentInset.top = 5
-        infoTextView.contentInset.right = 5
-        infoTextView.contentInset.bottom = 5
-        infoTextView.contentInset.left = 5
-        infoTextView.isEditable = false
-        infoTextView.isUserInteractionEnabled = false
-        infoTextView.tintColor = UIColor.black
-        infoTextView.backgroundColor = UIColor(white: 1, alpha: 0.6)
-        
-        self.userTextField.delegate = self
     }
     
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        unsubscribeFromKeyboardNotifications()
+
     }
     
     
@@ -108,125 +90,7 @@ class MakerViewController: UIViewController, UITextFieldDelegate {
         let greenHex = String(format: "%02X", gBase255)
         let blueHex = String(format: "%02X", bBase255)
         let hexCode = redHex + greenHex + blueHex
-        userTextField.text = hexCode
+        //        userTextField.text = hexCode // TODO: assign to eventual label
     }
-    
-    
-    func showWarning(reason: String) {
-        if reason == alertReason.invalidChar.rawValue {
-            warningLabel.text = "Please use: A-F, 0-9"
-        } else if reason == alertReason.maxChars.rawValue {
-            warningLabel.text = "Max 6 characters allowed"
-        } else {
-            // TODO: alert user
-            return
-        }
-        warningLabel.isHidden = false
-        warningLabel.alpha = 1.0
-        UIView.animate(withDuration: 3.0, animations: {
-            self.warningLabel.alpha = 0.0
-        }, completion: {(finished: Bool) in
-            if finished {
-                self.warningLabel.isHidden = true
-                self.warningLabel.alpha = 1.0
-            }
-        })
-        
-    }
-    
-    
-    // MARK: delegate methods
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        if string.count == 0 {
-            // Backspace key pressed
-            return true
-        }
-        
-        var oldText = textField.text!
-        oldText = oldText.uppercased()
-        let hexCharSet = CharacterSet(charactersIn: "1234567890ABCDEFabcdef")
-        
-        let filtered = string.components(separatedBy: hexCharSet).joined(separator: "")
-        
-        for c in string.unicodeScalars {
-            if !hexCharSet.contains(UnicodeScalar(c)) {
-                showWarning(reason: "invalidChar")
-                return false
-            }
-        }
-        
-        let uppercased = textField.text!.uppercased()
-        var newText = uppercased as NSString
-        newText = newText.replacingCharacters(in: range, with: string) as NSString
-        
-        if newText.length > 6 {
-            showWarning(reason: "maxChars")
-            return false
-        }
-        
-        if newText.length == 6 {
-            let redHex = Float("0x" + String(newText)[0...1])! / 255
-            let greenHex = Float("0x" + String(newText)[2...3])! / 255
-            let blueHex = Float("0x" + String(newText)[4...5])! / 255
-            UIView.animate(withDuration: 0.5, animations: {
-                self.redControl.setValue(redHex, animated: true)
-                self.greenControl.setValue(greenHex, animated: true)
-                self.blueControl.setValue(blueHex, animated: true)
-            })
-            
-            
-            view.backgroundColor = UIColor(red: CGFloat(redControl.value), green: CGFloat(greenControl.value), blue: CGFloat(blueControl.value), alpha: 1)
-            return true
-        }
-        
-        if newText.length > 5 {
-            showWarning(reason: "maxChars")
-            return false
-        }
-        
-        return !(string == filtered)
-    }
-    
-    
-    // MARK: Subscription
-    
-    func subscribeToKeyboardNotifications() {
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
-    }
-    
-    func unsubscribeFromKeyboardNotifications() {
-        
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
-    }
-    
-    
-    // MARK: Keyboard
-    
-    @objc func keyboardWillShow(_ notification:Notification) {
-        view.frame.origin.y -= getKeyboardHeight(notification)
-    }
-    
-    @objc func keyboardWillHide(_ notification:Notification) {
-        view.frame.origin.y = 0
-    }
-    
-    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-        
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-        return keyboardSize.cgRectValue.height
-    }
-    
 }
 
---
+++ ColorFull Picker/View/KeyboardButton.swift
@@ -0,0 +1,38 @@
+//
+//  KeyboardButton.swift
+//  ColorFull Picker
+//
+//  Created by Dani Springer on 15/07/2018.
+//  Copyright © 2018 Dani Springer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class KeyboardButton: UIButton {
+    
+    // MARK: Properties
+
+    let cornerRadius: CGFloat = 5.0
+    
+    
+    // MARK: Initialization
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        themeBorderedButton()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        themeBorderedButton()
+    }
+    
+    func themeBorderedButton() {
+        layer.masksToBounds = true
+        layer.cornerRadius = cornerRadius
+    }
+    
+}
+
+// TODO: riscrivi tu
