6f99a77ff8cb1c8a5982399c7aeb4ab45840d42b
Dani Springer <musicbyds@icloud.com>
2018-12-04 07:52:31 +0100

    fix: some SwiftLinting

--- ColorFull/Model/UIImage+Extensions.swift
+++ ColorFull/Model/UIImage+Extensions.swift
@@ -10,16 +10,16 @@ import UIKit
 
 extension UIImage {
     class func from(color: UIColor) -> UIImage {
-        let rect = CGRect(origin: CGPoint(x: 0, y:0), size: CGSize(width: 1, height: 1))
+        let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 1, height: 1))
         UIGraphicsBeginImageContext(rect.size)
         let context = UIGraphicsGetCurrentContext()!
-        
+
         context.setFillColor(color.cgColor)
         context.fill(rect)
-        
+
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
+
         return image!
     }
 }
