6bbe8aa9e3a3a4d54cb4ce02e9fd1bb1295464f4
Dani Springer <musicbyds@icloud.com>
2018-12-17 00:00:33 +0100

    fix: improve Constants

--- ColorFull/Model/Constants.swift
+++ ColorFull/Model/Constants.swift
@@ -2,13 +2,29 @@
 //  Constants.swift
 //  ColorFull
 //
-//  Created by Dani Springer on 11/29/18.
-//  Copyright © 2018 Dani Springer. All rights reserved.
+//  Created by Daniel Springer on 11/29/18.
+//  Copyright © 2018 Daniel Springer. All rights reserved.
 //
 
 import Foundation
 
 struct Constants {
-    static let hexPickerSelected = "hexPickerSelected"
-    static let colorKey = "color"
+    struct UserDef {
+        static let hexPickerSelected = "hexPickerSelected"
+        static let colorKey = "color"
+        static let isFirstLaunch = "isFirstLaunch"
+    }
+    struct Values {
+        static let numToHexFormatter = "%02X"
+        static let hexToNumFormatter = "0x"
+        static let rgbMax = 255.0
+    }
+    struct AppInfo {
+        static let bundleShort = "CFBundleShortVersionString"
+        static let appName = "ColorFull"
+        static let galleryLink = "photos-redirect://"
+        static let email = "musicbyds@icloud.com"
+        static let reviewLink = "https://itunes.apple.com/app/id1410565176?action=write-review"
+    }
+
 }
