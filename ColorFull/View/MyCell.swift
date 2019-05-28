4c8cf22791bda1f34d1ab6fdaf96ad5d3a5fa275
Daniel Springer <musicbyds@icloud.com>
2019-05-28 02:36:29 +0200

    fix: create custom view to support images in cells

---
+++ ColorFull/View/MyCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCell.swift
+//  ColorFull
+//
+//  Created by Daniel Springer on 5/28/19.
+//  Copyright © 2019 Dani Springer. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+
+    // MARK: Outlets
+
+    @IBOutlet weak var myImageView: UIImageView!
+
+}
