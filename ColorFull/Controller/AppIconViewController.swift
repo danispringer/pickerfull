9cd1def7cf20881f1c1ef22c3f61fe3c7dcabf0c
Daniel Springer <musicbyds@icloud.com>
2019-05-28 02:34:07 +0200

    fix: Create AppIconViewController

---
+++ ColorFull/Controller/AppIconViewController.swift
@@ -0,0 +1,108 @@
+//
+//  AppIconViewController.swift
+//  ColorFull
+//
+//  Created by Daniel Springer on 5/27/19.
+//  Copyright © 2019 Dani Springer. All rights reserved.
+//
+
+import UIKit
+
+
+class AppIconViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    // MARK: Outlets
+
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var myToolbar: UIToolbar!
+
+
+    // MARK: Properties
+
+    var textColor: UIColor! = nil
+    var backgroundColor: UIColor! = nil
+    let myDataSource = Array(1...14).map { "\($0)" }
+
+
+    // MARK: Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let darkMode = UserDefaults.standard.bool(forKey: Constants.UserDef.darkModeIsOn)
+
+        myToolbar.barTintColor = darkMode ? .black : .white
+        myToolbar.clipsToBounds = true
+        view.backgroundColor = darkMode ? .black : .white
+        textColor = darkMode ? .white : .black
+        backgroundColor = darkMode ? .black : .white
+
+    }
+
+
+    // MARK: Helpers
+
+
+    func updateIcon() {
+        let newIconNumberValue = UserDefaults.standard.integer(forKey: Constants.UserDef.selectedIcon)
+        print("newIconNumberValue: \(newIconNumberValue)")
+
+        guard UIApplication.shared.supportsAlternateIcons else {
+            print("NOTE: alternate icons not supported")
+            return
+        }
+
+        UIApplication.shared.setAlternateIconName("\(newIconNumberValue)") { error in
+            if let error = error {
+                print("App icon failed to change due to \(error.localizedDescription)")
+            } else {
+                print("app icon should now be updated")
+            }
+        }
+    }
+
+
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+    // MARK: TableView
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myDataSource.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let darkMode = UserDefaults.standard.bool(forKey: Constants.UserDef.darkModeIsOn)
+        tableView.backgroundColor = darkMode ? .black : .white
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CellID.cellID) as? MyCell
+        cell?.selectionStyle = .none
+        cell?.myImageView?.image = UIImage(named: "\(indexPath.row + 1)")
+        cell?.backgroundColor = darkMode ? .black : .white
+        cell?.contentView.backgroundColor = darkMode ? .black : .white
+        cell?.textLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+        cell?.accessoryType = .none
+
+        tableView.separatorColor = darkMode ? .white : .black
+
+        return cell ?? UITableViewCell()
+    }
+
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UserDefaults.standard.set(indexPath.row + 1, forKey: Constants.UserDef.selectedIcon)
+        print("Constants...selectedIcon): \(UserDefaults.standard.integer(forKey: Constants.UserDef.selectedIcon))")
+        updateIcon()
+        tableView.reloadData() // TODO: to remove previous checkmark. any cleaner way?
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+}
