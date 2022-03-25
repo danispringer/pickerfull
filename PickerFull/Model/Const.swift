62469bb927595be3f5f5856b3bc6585f7ec47e8b
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-03-25 00:38:51 -0400

    bump to 7.9 and fixes

--- PickerFull/Model/Const.swift
+++ PickerFull/Model/Const.swift
@@ -46,7 +46,7 @@ struct Const {
         static let bundleShort = "CFBundleShortVersionString"
         static let appName = "PickerFull"
         static let galleryLink = "photos-redirect://"
-        static let email = "dani.springer@icloud.com"
+        static let email = "00.segue_affix@icloud.com"
         static let reviewLink = "https://apps.apple.com/app/id1410565176?action=write-review"
         static let bundleAndRandom = "io.github.danispringer.Color-Picker.makeRandom"
         static let showAppsButtonTitle = "More Apps"
