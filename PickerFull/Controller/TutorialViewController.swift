7b67880ff763918a79ca6f63baa92e5131f4e4e3
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-08-28 20:08:13 -0400

    fixes

--- PickerFull/Controller/TutorialViewController.swift
+++ PickerFull/Controller/TutorialViewController.swift
@@ -20,23 +20,51 @@ class TutorialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        myTextView.text = """
+        let aString1 = NSAttributedString("(TIP: view this later by tapping ")
+        let infoIcon = NSTextAttachment(image: UIImage(systemName: "info.circle")!)
+        let infoiconString = NSAttributedString(attachment: infoIcon)
+        let aString2 = NSAttributedString(")")
+        let aString3 = NSAttributedString("\n\nSTEP #1: tap ")
+        let cameraIcon = NSTextAttachment(image: UIImage(systemName: "camera")!)
+        let cameraIconString = NSAttributedString(attachment: cameraIcon)
+        let aString4 = NSAttributedString(", then take or pick a photo")
+        let aString5 = NSAttributedString("\n\nSTEP #2: tap ")
+        let paintIcon = NSTextAttachment(image: UIImage(systemName: "paintpalette")!)
+        let paintIconString = NSAttributedString(attachment: paintIcon)
+        let aString6 = NSAttributedString("\n\nSTEP #3: tap ")
+        let penIcon = NSTextAttachment(image: UIImage(systemName: "eyedropper")!)
+        let penIconString = NSAttributedString(attachment: penIcon)
+        let aString7 = NSAttributedString("""
+        \n\nSTEP #4: drag-and-drop the circle over the wanted color
 
-        (TIP: view this later by tapping on ❔)
+        And you're done! You can now share your color by tapping
+        """)
+        let shareIcon = NSTextAttachment(image: UIImage(systemName: "square.and.arrow.up")!)
+        let shareIconString = NSAttributedString(attachment: shareIcon)
+        let aString8 = NSAttributedString("""
+        \n\n(You can now close the color picker page using the "X", and change or remove the photo \
+        by tapping
+        """)
+        let aString9 = NSAttributedString(")")
+        let aSpace = NSAttributedString(" ")
 
-        STEP #1: tap 📷, then take or pick a photo
+        let fullString = NSMutableAttributedString(string: "")
+        for attrString in [aString1, infoiconString, aString2, aString3, cameraIconString,
+                           aString4, aString5, paintIconString, aString6, penIconString,
+                           aString7, aSpace, shareIconString, aString8, aSpace,
+                           cameraIconString, aString9
+        ] {
+            fullString.append(attrString)
+        }
 
-        STEP #2: tap 🎨
+        let myFont: UIFont = UIFont.preferredFont(forTextStyle: .body)
 
-        STEP #3: tap the top-left 🖊️ icon
+        fullString.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.label,
+                                  NSAttributedString.Key.font: myFont],
+                                 range: (fullString.string as NSString).range(of: fullString.string))
 
-        STEP #4: drag-and-drop the circle over the wanted color
-
-        And you're done! You can now share your color by tapping on SHARE
-
-        (You can now close the color picker page using the "X", and change or remove the photo by tapping \
-        on 📷)
-        """
+        // draw the result in a label
+        myTextView.attributedText = fullString
 
     }
 
